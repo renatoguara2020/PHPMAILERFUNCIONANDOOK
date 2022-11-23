4e411e781f648f43b2a374a4c785b40e6a7ee217
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-23 15:10:02 -0300

    ok

--- sendEmail.php
+++ sendEmail.php
@@ -1,13 +1,24 @@
 <?php
 
+
   require('./PHPMailer-master/src/PHPMailer.php');
   require("./PHPMailer-master/src/SMTP.php");
   require('./PHPMailer-master/src/Exception.php');
+
+
+
+
+
+  
+  $nome = $_POST['nome'];
+
 
   use PHPMailer\PHPMailer\PHPMailer;
   use PHPMailer\PHPMailer\SMTP;
   use PHPMailer\PHPMailer\Exception;
 
+
+  
     $mail = new PHPMailer(true);
     $mail->IsSMTP(); // enable SMTP
 
@@ -25,9 +36,12 @@
     $mail->Body = "HELLO WORLD COM RENATO ALVES SOARES COM PHP MAILER E NATHANZINHO";
     $mail->AddAddress("renatoguara2019@yahoo.com");
 
+    
+
      if(!$mail->Send()) {
         echo "Mailer Error:  $mail->ErrorInfo ";
      } else {
         echo "<h1>Message has been sent to renatoguara2020</h1>";
      }
+
 ?>
