c688219354f034887be966773b71fb2044c527de
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-23 19:05:27 -0300

    ok

--- sendEmail.php
+++ sendEmail.php
@@ -1,62 +1,54 @@
 <?php
 
+require './PHPMailer-master/src/PHPMailer.php';
+require './PHPMailer-master/src/SMTP.php';
+require './PHPMailer-master/src/Exception.php';
 
-  require('./PHPMailer-master/src/PHPMailer.php');
-  require("./PHPMailer-master/src/SMTP.php");
-  require('./PHPMailer-master/src/Exception.php');
+$erros = [];
 
-
-
-$erros = array();
-if(isset($_POST['nome']) && $_POST['nome'] != '' && $_POST['nome'] != NULL){
-
-
-  $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
-    
-}else{
-
-  $erros[] = 'Digite seu nome';
+if (isset($_POST['nome']) && $_POST['nome'] != '' && $_POST['nome'] != null) {
+    $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS);
+} else {
+    $erros[] = 'Digite seu nome';
+    echo $erros;
 }
 
-if(isset($_POST['email']) && $_POST['email'] != '' && $_POST['email'] != NULL){
-
-  $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
-
-    
+if (
+    isset($_POST['email']) &&
+    $_POST['email'] != '' &&
+    $_POST['email'] != null
+) {
+    $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 }
-  
-  $assunto = $_POST['assunto'];
 
+$assunto = $_POST['assunto'];
 
-  use PHPMailer\PHPMailer\PHPMailer;
-  use PHPMailer\PHPMailer\SMTP;
-  use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
+$mail = new PHPMailer(true);
+$mail->IsSMTP(); // enable SMTP
 
-  
-    $mail = new PHPMailer(true);
-    $mail->IsSMTP(); // enable SMTP
+$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPAuth = true; // authentication enabled
+$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+$mail->Host = 'smtp.gmail.com';
+//  $email->SMTPAuth = true;
+$mail->Port = 465; // or 587
+$mail->IsHTML(true);
+$mail->Username = 'renatoguara2020@gmail.com';
+$mail->Password = 'gpg';
+$mail->SetFrom('renatoguara2020@gmail.com');
+$mail->Subject = 'PHP MAILER';
+$mail->Body =
+    'HELLO WORLD COM RENATO ALVES SOARES COM PHP MAILER E NATHANZINHO';
+$mail->AddAddress('renatoguara2019@yahoo.com');
 
-    $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
-    $mail->SMTPAuth = true; // authentication enabled
-    $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
-    $mail->Host = "smtp.gmail.com";
-    //  $email->SMTPAuth = true;
-    $mail->Port = 465; // or 587
-    $mail->IsHTML(true);
-    $mail->Username = "renatoguara2020@gmail.com";
-    $mail->Password = "gpg";
-    $mail->SetFrom("renatoguara2020@gmail.com");
-    $mail->Subject = "PHP MAILER";
-    $mail->Body = "HELLO WORLD COM RENATO ALVES SOARES COM PHP MAILER E NATHANZINHO";
-    $mail->AddAddress("renatoguara2019@yahoo.com");
-
-    
-
-     if(!$mail->Send()){ 
-        echo "Mailer Error:  $mail->ErrorInfo";
-     }else{
-        echo "Successfully Send";
-     }
+if (!$mail->Send()) {
+    echo "Mailer Error:  $mail->ErrorInfo";
+} else {
+    echo 'Successfully Send';
+}
 
 ?>
