e33ed5f38ee52b12ca09f3201b882222f1fa40de
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-24 12:30:25 -0300

    ok

---
+++ authentication.php
@@ -0,0 +1,10 @@
+<?php 
+  define('ADMIN_LOGIN','root'); 
+  define('ADMIN_PASSWORD','078800'); // Could be hashed too.
+  
+  if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) || ($_SERVER['PHP_AUTH_USER'] != ADMIN_LOGIN) || ($_SERVER['PHP_AUTH_PW'] != ADMIN_PASSWORD)) { 
+    header('HTTP/1.1 401 Unauthorized'); 
+    header('WWW-Authenticate: Basic realm="Password For Blog"'); 
+    exit("Access Denied: Username and password required."); 
+  } 
+?>
