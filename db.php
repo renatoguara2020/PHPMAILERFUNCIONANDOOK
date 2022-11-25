2bab927397c24039f3441df60507dc1ed6f91ba8
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-25 07:54:33 -0300

    ok

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+    $hostname = "localhost";
+    $username = "root";
+    $password = "";
+    try {
+        $connection = new PDO("mysql:host=$hostname;dbname=php_crud", $username, $password);
+        // set the PDO error mode to exception
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        echo "Database connection failed: " . $e->getMessage();
+    }
+?>
